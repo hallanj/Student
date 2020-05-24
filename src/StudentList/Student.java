aec7039d81795129b57d6dbd201959cc11f94fe7
hallanj <hallanj@sheridancollege.ca>
2020-05-24 17:46:09 -0400

    First Commit

---
+++ src/StudentList/Student.java
@@ -0,0 +1,33 @@
+
+
+package StudentList;
+
+/**
+ *This class represents students in our application
+ * @author jasmeen
+ */
+public class Student {
+
+    private String name;
+    public Student(String name){
+    this.name=name;
+    }
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
